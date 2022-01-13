f31a3bfc0e0c579bdf8f3be2904ff17b8451779d
Sufyan Iqbal <Sufyan.Iqbal@activekeysolutions.com>
2022-01-13 12:59:49 +0500

    server side code refectored

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
+
   def def new
     @comment = Comment.new
   end
 
   def create
-    comment = Comment.new(comment_params)
+    comment = current_user.build_comment(comment_params)
     comment.user_id = current_user.id
     respond_to do |format|
       if comment.save
@@ -12,20 +13,16 @@ class CommentsController < ApplicationController
           render turbo_stream: [
             turbo_stream.append("post_#{params[:post_id]}_comments", partial: "comments/comment", locals: {comment: comment} )
           ]
-      end
-
-        format.html { redirect_to post_url(comment.post), notice: "comment added" }
+        end
       else
         format.html { redirect_to post_url(params[:post_id]), notice: "comment not added" }
       end
     end
-
   end
 
   private
 
-    def comment_params
-      params.require(:comment).permit(:user_id, :post_id, :message)
-    end
-
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id, :message)
+  end
 end
